cdfb08491c069b9dbee23922ee2ac420e647c39a
jessicaggan <jessica.ganadhi@binus.ac.id>
2021-12-22 16:05:52 +0700

    penambahan function perkalian

--- calc.py
+++ calc.py
@@ -4,13 +4,16 @@ def penambahan(a,b):
 def pengurangan(a,b):
 	total=a-b
 	return total
-def pembagian(a/b):
+def pembagian(a,b):
 	total=a/b
+	return total
+def perkalian(a,b):
+	total=a*b
 	return total
 
 def main():
 	print(penambahan(10,5))
 	print(pengurangan(10,5))
 	print(pembagian(10,5))
-
+	print(perkalian(10,5))
 main()
